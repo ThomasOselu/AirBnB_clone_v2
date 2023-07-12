3a2c97e9d510bffddf57f892bf47093edbaaaa66
ThomasOselu <thomasoselu1@gmail.com>
2023-07-12 09:29:42 +0000

    airbnb deployment

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,26 +3,16 @@
 create tarball artifact of static files in local
 '''
 
-from fabric.api import local
-from datetime import datetime
-import os
-
+from fabric.api import *
+import time
+from datetime import date
 
 def do_pack():
-    '''function:
-    generates a .tgz archive from the contents of the web_static folder
-    '''
-    now = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-
-    artifact = f'versions/web_static_{now}.tgz'
-    print(f'Packing web_static to {artifact}')
-    if not os.path.exists('versions'):
-        local('mkdir -p versions')
-
-    fab_stat = local(f'tar -cvzf {artifact} web_static')
-    if fab_stat.succeeded:
-        size = os.path.getsize(artifact)
-        print(f'web_static packed: {artifact} -> {size}Bytes')
-        return artifact
-    else:
+    timestamp = time.strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{:s}.tgz web_static/".
+              format(timestamp))
+        return ("versions/web_static_{:s}.tgz".format(timestamp))
+    except:
         return None
